a4eae19b6b162ba79e0f063639bcfe7571c7a74a
Miguelangel <silvermiguel96@gmail.com>
2020-02-11 22:11:47 -0500

    used props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// function Hello (props) {
-//   return <h2>{props.title}</h2>
-// }
+class Text extends Component {
+  render() {
+    const texoSegundoBool = this.props.isActivated ? 'On' : 'Off'
+    const mappedNumbers = this.props.arrayOfNumbers.map(n => n * 2)
 
-class Hello extends Component {
-  render () {
-    return <h2>{this.props.title}</h2>
+    return (
+      <div>
+        <p>{mappedNumbers.join(', ')}</p>
+        <p>{texoSegundoBool}</p>
+        <p>{this.props.number}</p>
+      </div>
+    )
   }
 }
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" />
-          <Hello title='Hello from props' />
+          <div>
+            <Text
+              arrayOfNumbers={[2, 3, 10]}
+              objectWithInfo={{ key: "fistValue", key2: "otherValue" }}
+              isActivated
+              number={2}
+            />
+
+          </div>
         </div>
       </div>
     )
