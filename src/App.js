ef34e2ebcbd7bac10c91eb7a26682235c4883619
Miguelangel <silvermiguel96@gmail.com>
2020-02-11 21:53:26 -0500

    created component React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function Hello (props) {
+//   return <h2>{props.title}</h2>
+// }
+
+class Hello extends Component {
+  render () {
+    return <h2>{this.props.title}</h2>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" />
+          <Hello title='Hello from props' />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
