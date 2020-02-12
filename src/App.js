526bff7f776d0201afa8bf68067d029236f5d844
Miguelangel <silvermiguel96@gmail.com>
2020-02-11 22:56:53 -0500

    Created state

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class Text extends Component {
-  render() {
+class Contador extends Component {
+  constructor () {
+    super()
+    this.state = { contador: 1}
+  }
 
-    const {
-      arrayOfNumbers,
-      objectWithInfo,
-      isActivated,
-      number,
-      title
-    } = this.props
-
-    const texoSegundoBool = isActivated ? 'On' : 'Off'
-    const mappedNumbers = arrayOfNumbers.map(n => n * 2)
-
-    return (
-      <div>
-        {title}
-        <p>{mappedNumbers.join(', ')}</p>
-        <p>{texoSegundoBool}</p>
-        <p>{number}</p>
-        <p>{objectWithInfo.key2}</p>
-      </div>
-    )
+  render () {
+    return <span>{this.state.contador}</span>
   }
 }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" />
-          <div>
-            <Text
-              arrayOfNumbers={[2, 3, 10]}
-              objectWithInfo={{ key: "fistValue", key2: "otherValue" }}
-              isActivated
-              number={2}
-              title={<h2>Este es un titulo</h2>}
-            />
-
-          </div>
-        </div>
+        <p>Primer componente con state</p>
+        <Contador />
       </div>
     )
   }
 }
-
 export default App;
